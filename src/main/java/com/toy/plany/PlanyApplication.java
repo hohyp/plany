8484b14fe019fcc8a7714624a74ffa6068c169f3
박호현 <hamoris@naver.com>
2022-01-20 10:51:12 +0900

    init

---
+++ src/main/java/com/toy/plany/PlanyApplication.java
@@ -0,0 +1,13 @@
+package com.toy.plany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toy/plany/PlanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.plany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
